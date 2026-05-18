5ab98c59107ff77bdaa06092287e4bbb97501217
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-05-18 17:29:56 +0530

    initial commit

---
+++ find i bigger than15.py
@@ -0,0 +1,8 @@
+i = int(input('Enter a number:'))
+if i < 15:
+    print(i, 'Is smaller than 15')
+    print('Im in if block')
+else:
+    print(i, 'Is bigger than 15')
+    print('im in else block')
+print('I am not in if and else block')
