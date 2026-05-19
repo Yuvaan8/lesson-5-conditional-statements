4c9a415fc11c76240129014b171679a3d84e5585
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-05-19 16:10:45 +0530

    initial commti

---
+++ temperature.py
@@ -0,0 +1,5 @@
+temperature = int(input('Enter the temperature in Celsius:'))
+if temperature > 25:
+    print('The weather is suitable for wearing light clothes')
+else:
+    print('The weather is not suitable for wearing light clothes')
